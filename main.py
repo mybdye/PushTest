0ad896900a8a8e8cdf81f2bf659830de4434825e
mybdye <mybdye@gmail.com>
2022-03-22 16:50:27 +0800

    update

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import requests
 import os
 
 barkKey = str(os.environ['BARKKEY'])
+print('barkKey', barkKey)
 session = HTMLSession()
 header = {
 'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36'
