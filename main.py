9bbe6773d67a55e8b0172244c2e9017a4bb19f2e
mybdye <mybdye@gmail.com>
2022-03-22 16:59:48 +0800

    update

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import requests
 import os
 
 barkKey = os.environ['BARKKEY']
-print('barkKey:', barkKey)
 session = HTMLSession()
 header = {
 'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36'
@@ -15,15 +14,15 @@ product = req.html.xpath("//div[@id='product1']/header/span[@id='product1-name']
 # 取得产品余量
 qty = req.html.xpath("//div[@id='product1']/header/span[@class='qty']/text()")[0]
 
+# 打印 产品信息
+print(product, qty)
 
 # bark 推送
-title = 'Vir 监控'
-body = product +' 补货了\n' + qty
-print(body)
-
 if barkKey !=' ':
     barkUrl = 'https://api.day.app/'+barkKey
     if int(qty.split(' ')[0]) != 0:
+        title = 'Vir 监控'
+        body = product + ' 补货了\n' + qty
         requests.get(url=f'{barkUrl}/{title}/{body}?isArchive=1')
     else:
         print('未补货')
