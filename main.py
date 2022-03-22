38c4383d4f105d74658f031086016d7a805c2c07
mybdye <mybdye@gmail.com>
2022-03-22 16:54:47 +0800

    update

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from requests_html import HTMLSession
 import requests
 import os
 
-barkKey = str(os.environ['BARKKEY'])
-print('barkKey', barkKey)
+barkKey = os.environ['BARKKEY']
+print('barkKey:', barkKey)
 session = HTMLSession()
 header = {
 'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36'
@@ -15,15 +15,18 @@ product = req.html.xpath("//div[@id='product1']/header/span[@id='product1-name']
 # 取得产品余量
 qty = req.html.xpath("//div[@id='product1']/header/span[@class='qty']/text()")[0]
 
-if int(qty.split(' ')[0]) !=0:
-    # bark 推送
-    title = 'Vir 监控'
-    body = product +' 补货了\n' + qty
-    if barkKey !=' ':
-        barkUrl = 'https://api.day.app/'+barkKey
+
+# bark 推送
+title = 'Vir 监控'
+body = product +' 补货了\n' + qty
+print(body)
+
+if barkKey !=' ':
+    barkUrl = 'https://api.day.app/'+barkKey
+    if int(qty.split(' ')[0]) != 0:
         requests.get(url=f'{barkUrl}/{title}/{body}?isArchive=1')
     else:
-        print('BARKKEY 未填写')
-    print(body)
+        print('未补货')
 else:
-    print('未补货')
+    print('BARKKEY 未填写')
+
